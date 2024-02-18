69a1d86e04560f19de5ee034048e3b96536e7bfd
Pranshu <110905434+pranshu314@users.noreply.github.com>
2024-02-18 16:29:39 +0530

    Update tmux_switch.sh

--- tmux_switch.sh
+++ tmux_switch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-SESSION=$(tmux list-sessions -F \#S | gum filter --placeholder "Pick session...")
+#SESSION=$(tmux list-sessions -F \#S | gum filter --placeholder "Pick session...")
+SESSION=$(tmux list-sessions -F \#S | fzf)
 tmux switch-client -t $SESSION || tmux attach -t $SESSION
